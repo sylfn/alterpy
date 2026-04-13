6ad0dbea2e72556a8a7ebbbb5f9f906cb01c9ed7
Yuki Sireneva <yuki.utk8g@gmail.com>
2026-04-13 16:56:55 +0300

    utils.help: Remove `path`

--- utils/help.py
+++ utils/help.py
@@ -7,23 +7,23 @@ import typing
 import re
 
 
-def entry(lang: str, path: str = '', name: str = '') -> str:
-    return (f'https://yuki0iq.github.io/alterpy/{path}{lang}#{name}').replace('\\', '\\\\').replace(')', '\\)')
+def entry(lang: str, name: str = '') -> str:
+    return (f'https://sylfn.github.io/alterpy/{lang}#{name}').replace('\\', '\\\\').replace(')', '\\)')
 
 
-def link(lang: str, path: str = '', name: str = '') -> str:
-    ent = entry(lang, path, name)
+def link(lang: str, name: str = '') -> str:
+    ent = entry(lang, name)
     return f'[{utils.str.escape(name or "Help")}]({ent})'
 
 
-def forward_handler(path: str) -> typing.Callable[[utils.cm.CommandMessage], typing.Awaitable[None]]:
+def forward_handler() -> typing.Callable[[utils.cm.CommandMessage], typing.Awaitable[None]]:
     async def on_help(cm: utils.cm.CommandMessage) -> None:
         # TODO add check exists.
-        await cm.int_cur.reply(link(cm.lang, path, cm.arg))
+        await cm.int_cur.reply(link(cm.lang, cm.arg))
     return on_help
 
 
-def on_reverse_help_impl(handlers: list[typing.Any], arg: str, help_cmds: list[str], path: str, lang: str) -> str:
+def on_reverse_help_impl(handlers: list[typing.Any], arg: str, help_cmds: list[str], lang: str) -> str:
     if not arg:
         return f"Type `{help_cmds} [command]` to view help page for command"
 
@@ -39,26 +39,30 @@ def on_reverse_help_impl(handlers: list[typing.Any], arg: str, help_cmds: list[s
     if not help_pages_list:
         return f"Could not match `{arg}`"
 
-    res = "Help entries:\n" + '\n'.join(map(lambda x: link(lang, path, x), help_pages_list))
+    res = "Help entries:\n" + '\n'.join(map(lambda x: link(lang, x), help_pages_list))
     print(res)
     return res
 
 
-def reverse_handler(handlers: list[typing.Any], help_cmds: list[str], path: str) -> typing.Callable[[utils.cm.CommandMessage], typing.Awaitable[None]]:
+def reverse_handler(handlers: list[typing.Any], help_cmds: list[str]) -> typing.Callable[[utils.cm.CommandMessage], typing.Awaitable[None]]:
     async def on_help(cm: utils.cm.CommandMessage) -> None:
-        await cm.int_cur.reply(on_reverse_help_impl(handlers, cm.arg, help_cmds, path, cm.lang))
+        await cm.int_cur.reply(on_reverse_help_impl(handlers, cm.arg, help_cmds, cm.lang))
     return on_help
 
 
-def add(handlers: list[typing.Any], man_cmds: list[str] = ["man"], help_cmds: list[str] = ["help"], path: str = "") -> None:
-    if path:
-        path += '_'
-    help_page = 'help'
+def add(handlers: list[typing.Any], man_cmds: list[str] = ["man"], help_cmds: list[str] = ["help"]) -> None:
     handlers.extend([
-        utils.ch.CommandHandler(name=name, pattern=utils.regex.pre(utils.regex.union(commands)), help_page=help_page, handler_impl=handler, is_prefix=True, is_arg_current=True)
+        utils.ch.CommandHandler(
+            name=name,
+            pattern=utils.regex.pre(utils.regex.union(commands)),
+            help_page='help',
+            handler_impl=handler,
+            is_prefix=True,
+            is_arg_current=True
+        )
         for name, commands, handler in [
-            (f'man{path}',  man_cmds,  forward_handler(path)),
-            (f'help{path}', help_cmds, reverse_handler(handlers, help_cmds, path))
+            (f'man',  man_cmds,  forward_handler()),
+            (f'help', help_cmds, reverse_handler(handlers, help_cmds))
         ]
     ])
 
