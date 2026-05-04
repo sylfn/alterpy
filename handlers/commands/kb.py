a757ef7724f19a081fa2ca5512faab21a504dee5
Yuki Sireneva <sylfn@disroot.org>
2026-05-04 19:28:31 +0300

    Remove qwerty-jcuken layout changer

--- handlers/commands/kb.py
+++ handlers/commands/kb.py
@@ -8,11 +8,6 @@ import utils.common
 handler_list = []
 
 
-async def on_layout(cm: utils.cm.CommandMessage) -> None:
-    if cm.arg:
-        await cm.int_cur.reply(utils.str.escape(utils.str.change_layout(cm.arg)))
-
-
 async def on_me(cm: utils.cm.CommandMessage) -> None:
     if cm.arg:
         msg = f"\\* _{cm.sender.get_display_name()} {utils.str.escape(cm.arg)}_"
@@ -24,15 +19,6 @@ async def on_me(cm: utils.cm.CommandMessage) -> None:
             await cm.int_cur.delete()
         except:
             await cm.int_cur.reply("Can't delete message — no permission")
-
-
-handler_list.append(utils.ch.CommandHandler(
-    name='layout',
-    pattern=utils.regex.cmd(utils.regex.unite('kb', 'ли', 'layout', 'дфнщге', 'раскладка', 'hfcrkflrf', 'рас', 'hfc')),
-    help_page="keyboard",
-    handler_impl=on_layout,
-    is_prefix=True
-))
 
 handler_list.append(utils.ch.CommandHandler(
     name="me",
--- utils/str.py
+++ utils/str.py
@@ -4,21 +4,6 @@ import urllib.parse
 import typing
 
 
-def change_layout(s: str) -> str:
-    """alternate between QWERTY and JCUKEN"""
-    en = r"""`~!@#$^&qwertyuiop[]\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?"""
-    ru = r"""ёЁ!"№;:?йцукенгшщзхъ\ЙЦУКЕНГШЩЗХЪ/фывапролджэФЫВАПРОЛДЖЭячсмитьбю.ЯЧСМИТЬБЮ,"""
-    fr, to = en + ru, ru + en
-
-    res = []
-    for c in s:
-        if c in fr:
-            res.append(to[fr.index(c)])
-        else:
-            res.append(c)
-    return ''.join(res)
-
-
 def is_eng(s: str) -> bool:
     return not s or 'a' <= s[0].lower() <= 'z'
 
