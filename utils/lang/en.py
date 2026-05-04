12c5cd2c3b7e9dc8633ac6490fd1a1a034f9119e
Yuki Sireneva <sylfn@disroot.org>
2026-05-04 20:30:20 +0300

    Remove lemminflect

--- utils/lang/en.py
+++ utils/lang/en.py
@@ -1,19 +1,12 @@
-import lemminflect
-import typing
-
-
 def try_verb_past(w: str, p: int) -> str:
-    return inflect(w, 'VBD') if w.isalpha() else w
-
+    return w
 
 def inflect(s: str, form: str, _pn: int | None = None) -> str:
     ret = lemminflect.getInflection(s, form)[0]
     assert isinstance(ret, str)
     return ret
 
-
-def agree_with_number(s: str, num: int, _: typing.Any) -> str:
+def agree_with_number(s: str, num: int, _: any) -> str:
     if type(num) != int or abs(num) > 1:
         return s + "s"
     return s
-
