98cad8b294147c5edbbd3735031cd272f28baa7d
Yuki Sireneva <sylfn@disroot.org>
2026-05-04 19:34:20 +0300

    utils.config: remove dead code

--- utils/config.py
+++ utils/config.py
@@ -1,22 +1,10 @@
 import pytomlpp
-import typing
 import os
 
-
-def to_path(name: str) -> str:
-    return f"./config/{name}.toml"
-
-
-def load(name: str) -> typing.Dict[typing.Any, typing.Any]:
-    """get config by filename"""
-    path = to_path(name)
+def load(name: str) -> dict:
+    path = f"./config/{name}.toml"
     if not os.path.exists(path):
         with open(path, "w") as f:
             pass
     return pytomlpp.load(path)
-
-
-def save(name: str, conf: typing.Dict[typing.Any, typing.Any]) -> None:
-    """save config by filename"""
-    pytomlpp.dump(conf, to_path(name))
 
