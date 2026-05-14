d8e2aca8155abad674b8ffc2e39110306c17998e
Yuki Sireneva <sylfn@disroot.org>
2026-05-14 23:38:12 +0300

    remove da/net/dura

--- commands/kek.py
+++ commands/kek.py
@@ -1,46 +1,9 @@
 import utils.command
-import utils.rand
 import utils.regex
-import random
-
-
-async def on_da(cm: utils.command.Message) -> None:
-    if random.randint(1, 4) == 1:
-        await cm.int_cur.reply(
-            utils.rand.weighted([
-                (1, 'сковорода'),
-                (1, 'лабуда'),
-                (1, 'винда'),
-            ])
-        )
-
-
-async def on_net(cm: utils.command.Message) -> None:
-    if random.randint(1, 4) == 1:
-        await cm.int_cur.reply(
-            utils.rand.weighted([
-                (1, 'солнышка ответ'),
-                (1, 'лунышка ответ'),
-            ])
-        )
-
-
-async def on_dura(cm: utils.command.Message) -> None:
-    if random.randint(1, 4) == 1:
-        await cm.int_cur.reply('а может ты 🤨?')
-
 
 async def on_spok(cm: utils.command.Message) -> None:
     await cm.int_cur.reply('Cладких снов 🥺')
 
-
 handler_list = [
-    utils.command.Handler(name=name, pattern=utils.regex.raw(pat), help_page="kek", handler_impl=handler)
-    for name, pat, handler in [
-        ("да", "\\bда$", on_da),
-        ("нет", "\\bнет$", on_net),
-        ("дура", "\\bдура$", on_dura),
-        ("спокойной ночи", "^((всем ){0,1}спокойной ночи)", on_spok),
-    ]
+    utils.command.Handler(name="спокойной ночи", pattern=utils.regex.raw("((всем ){0,1}спокойной ночи)"), help_page="kek", handler_impl=on_spok)
 ]
-
